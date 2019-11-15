53696a36103d69f742840abf14dfbaeceea4b2b7
Dair Baidauletov <dair.baidauletov@vaimo.com>
2019-11-15 22:26:26 +0200

    Installed Vue 3

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Main msg="Point Of No Return"/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 60px auto 0;
+  max-width: 1080px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  /*
+  #F44336
+  #9E9E9E
+  */
+}
+</style>
