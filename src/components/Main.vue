8bc805eac45c7825e91d47564f00cc064ed24d6c
Dair Baidauletov <dair.baidauletov@gmail.com>
2019-11-16 10:40:12 +0200

    Dummy color picker functionality

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,16 +7,69 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Upload image</h3>
-    <input type="file" />
+    <input type="file" v-on:change="handleImage" />
+		<canvas id="uploadedImage" v-on:mousemove="getCurrentColor"></canvas>
+		<p>Current color: {{hex}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+	name: 'Main',
+	data: function () {
+    return {
+			hex: 0
+		}
+	},
   props: {
-    msg: String
-  }
+		msg: String
+	},
+  methods: {
+		handleImage: function (e) {
+			let canvas = document.getElementById('uploadedImage');
+			let ctx = canvas.getContext("2d");
+			let reader = new FileReader();
+			reader.onload = function(event){
+					let img = new Image();
+					img.onload = function(){
+							canvas.style.width = '100%';
+							canvas.width = img.width;
+							canvas.height = img.height;
+							ctx.drawImage(img,0,0);
+					}
+					img.src = event.target.result;
+			}
+			reader.readAsDataURL(e.target.files[0]);		
+		},
+		getCurrentColor: function (e) {
+			let canvas = document.getElementById('uploadedImage');
+
+			var pos = this.findPos(canvas);
+			var x = e.pageX - pos.x;
+			var y = e.pageY - pos.y;
+			var c = canvas.getContext('2d');
+			var p = c.getImageData(x, y, 1, 1).data; 
+			var hex = "#" + ("000000" + this.rgbToHex(p[0], p[1], p[2])).slice(-6);
+			this.hex = hex
+		},
+		findPos: function (obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+						curtop += obj.offsetTop;
+						obj = obj.offsetParent;
+        } while (obj);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+		},
+		rgbToHex: function (r, g, b) {
+			if (r > 255 || g > 255 || b > 255)
+					throw "Invalid color component";
+				return ((r << 16) | (g << 8) | b).toString(16);
+		}
+	}
 }
 </script>
 
