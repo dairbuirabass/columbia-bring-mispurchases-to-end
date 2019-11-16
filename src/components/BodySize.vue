52358dd3a128a84b338d6d1ef615e240de8fa9d4
Dair Baidauletov <dair.baidauletov@gmail.com>
2019-11-16 15:12:53 +0200

    Added BodySize component

---
+++ src/components/BodySize.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="body-size">
+     <!-- http://episodic-vuejs.s3-website.eu-central-1.amazonaws.com -->
+    <iframe src="http://files.is.tue.mpg.de/erachlin/sina/body_sizing_site/" height="720" width="792" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BodySize'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  iframe {
+    width: 792px;
+    height: 720px;
+  }
+</style>
